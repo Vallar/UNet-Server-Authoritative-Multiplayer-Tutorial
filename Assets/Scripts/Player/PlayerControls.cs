072a85d51e1817ff4d1b9c255b5a5d13ff67cb68
Mohamed <elsamahy.mohamed@gmail.com>
2017-09-26 08:49:41 +0200

    Intial Commit - Part 1 of the series.

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerControls : NetworkBehaviour {
+
+    private PlayerMovement movement;
+
+    private void Awake()
+    {
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if(isLocalPlayer)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
+
+            movement.MovementPrediction(x, y);
+            movement.Cmd_ServerMovePlayer(x, y);
+        }
+    }
+}
